d35d38ebaa5e37ced5e741e56f5dca0d6d0ccbaf
Clement Morisset <morissetcl87@gmail.com>
2019-07-13 09:14:14 +0200

    add hp background

--- components/Connexion.js
+++ components/Connexion.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, Alert, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, Alert, TouchableOpacity, ActivityIndicator, ImageBackground } from 'react-native';
 import { Item, Form, Input, Label } from "native-base";
 import * as firebase from "firebase";
 import Prayers from './Prayers'
+import { showMessage } from "react-native-flash-message";
 
 export default class Connexion extends React.Component {
   constructor(props) {
@@ -17,19 +18,27 @@ export default class Connexion extends React.Component {
   }
 
   SignUp = (email, password) => {
-    try {
-    firebase.auth().createUserWithEmailAndPassword(email,password)
-      .then(() => this.setState({ logged: true }))
-      .catch(error => {
-        switch(error.code) {
-          case 'auth/email-already-in-use':
-                Alert.alert('Email already in use !')
-                break;
-       }
-     })
-   } catch (error){
-       alert("Error : ", error);
-   }
+    if (this.state.email.length !== 0 && this.state.password !== 0 &&  this.state.username !== 0) {
+      try {
+      firebase.auth().createUserWithEmailAndPassword(email,password)
+        .then(() => this.setState({ logged: true }))
+        .catch(error => {
+          switch(error.code) {
+            case 'auth/email-already-in-use':
+                  Alert.alert('Email already in use !')
+                  break;
+         }
+       })
+     } catch (error){
+         alert("Error : ", error);
+     }
+    } else {
+      showMessage({
+        message: 'Merci de remplir tous les champs pour vous inscrire.',
+        type: 'warning',
+        icon: 'warning'
+      });
+    }
   };
 
   Login = (email, password) => {
@@ -79,40 +88,52 @@ export default class Connexion extends React.Component {
             <Prayers navigation={ this.props.navigation } currentUserEmail={ firebase.auth().currentUser.email } username={this.state.username}/>
             :
             <View>
-              <Form>
-                <Item floatingLabel>
-                  <Label>Username</Label>
-                  <Input
-                    autoCapitalize="none"
-                    autoCorrect={false}
-                    onChangeText={username => this.setState({username: username })}
-                  />
-                </Item>
-                <Item floatingLabel>
-                  <Label>Email</Label>
-                  <Input
-                    autoCapitalize="none"
-                    autoCorrect={false}
-                    onChangeText={email => this.setState({ email })}
-                  />
-                </Item>
-                <Item floatingLabel style={styles.connexion_input}>
-                  <Label>Mot de passe</Label>
-                  <Input
-                    secureTextEntry={true}
-                    autoCapitalize="none"
-                    autoCorrect={false}
-                    onChangeText={password => this.setState({ password })}
-                  />
-                </Item>
-                <TouchableOpacity style={styles.bouton} onPress={() => this.SignUp(this.state.email, this.state.password)} >
-                  <Text>Inscription</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.bouton_transparent} onPress={() => this.Login(this.state.email, this.state.password)}>
-                  <Text>Déja inscrit ?</Text>
-                </TouchableOpacity>
+              <ImageBackground source = {require('../assets/prayer_red.jpg')} style = {styles.image} />
+              <View style={styles.connexion_from}>
+                <Text style={{color: 'white', fontSize: 30, textAlign: 'center', marginLeft: 30}}>Bless Me.</Text>
+                <Text style={{color: 'white', fontSize: 18, textAlign: 'center', marginTop: 30, marginBottom: 80, marginLeft: 30}}>
+                  Connectez-vous et commencez à prier pour ceux qui en on besoin.
+                </Text>
+              </View>
+              <View style={styles.form_wrapper}>
+
+                <Form>
+                  <Item floatingLabel>
+                    <Label>Pseudonyme</Label>
+                    <Input
+                      autoCapitalize="none"
+                      autoCorrect={false}
+                      onChangeText={username => this.setState({username: username })}
+                    />
+                  </Item>
+                  <Item floatingLabel>
+                    <Label>Email</Label>
+                    <Input
+                      autoCapitalize="none"
+                      autoCorrect={false}
+                      onChangeText={email => this.setState({ email })}
+                    />
+                  </Item>
+                  <Item floatingLabel style={styles.connexion_input}>
+                    <Label>Mot de passe</Label>
+                    <Input
+                      secureTextEntry={true}
+                      autoCapitalize="none"
+                      autoCorrect={false}
+                      onChangeText={password => this.setState({ password })}
+                    />
+                  </Item>
+                  <View style={styles.boutons_wrapper}>
+                    <TouchableOpacity style={styles.bouton} onPress={() => this.SignUp(this.state.email, this.state.password)} >
+                      <Text style={{color: 'white'}}>Inscription</Text>
+                    </TouchableOpacity>
+                  </View>
               </Form>
             </View>
+            <TouchableOpacity onPress={() => this.SignUp(this.state.email, this.state.password)} >
+              <Text style={{color: 'white', textAlign: 'center'}}>Déja inscrit ?</Text>
+            </TouchableOpacity>
+          </View>
           }
           </View>
           :
@@ -124,12 +145,37 @@ export default class Connexion extends React.Component {
 }
 
 const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    position: 'absolute',
+    width: '100%',
+    height: '120%',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  },
+  boutons_wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginLeft: '5%',
+    marginTop: 15
+  },
+  form_wrapper: {
+    backgroundColor: 'white',
+    paddingLeft: '5%',
+    paddingRight: '10%',
+    paddingBottom: '10%',
+    margin: '10%',
+    borderRadius: 10
+  },
+  connexion_from: {
+    paddingTop: '20%',
+    paddingLeft: 20,
+    paddingRight: 50
+  },
   container: {
     height: '100%',
     backgroundColor: '#FFFFFF' // background tab color
-  },
-  coucou: {
-    width: '80%',
   },
   loader: {
     color:"#0000ff",
@@ -146,20 +192,18 @@ const styles = StyleSheet.create({
     display: 'flex',
     alignItems: 'center',
     width: '80%',
-    marginLeft: '10%',
     marginBottom: '2%',
     borderRadius: 30,
-    borderWidth: 2,
-    color: 'red'
+    borderWidth: 2
+
   },
   bouton: {
     borderColor: 'transparent',
-    backgroundColor: '#01676b',
+    backgroundColor: '#ff8b6a',
     padding: 15,
     display: 'flex',
     alignItems: 'center',
     width: '80%',
-    marginLeft: '10%',
     marginBottom: '2%',
     borderRadius: 30,
     borderWidth: 2
