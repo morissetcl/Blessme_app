d655b7f7e2d0fceb7dc8d9cc9b7336f357628d98
Clement Morisset <morissetcl87@gmail.com>
2019-07-21 09:38:18 +0200

    add notifications

---
+++ services/notifications.js
@@ -0,0 +1,29 @@
+import { AsyncStorage } from 'react-native';
+import { Permissions, Notifications } from 'expo';
+
+const PUSH_ENDPOINT = "https://blessme-serveur.herokuapp.com/api/v1/expo_tokens";
+
+export default registerForNotifications = async (email) => {
+  const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+  if (status !== 'granted') {
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    if (status !== 'granted') {
+      return;
+    }
+  }
+
+  const token = await Notifications.getExpoPushTokenAsync();
+
+  fetch(PUSH_ENDPOINT, {
+                          method: 'POST',
+                          headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json',
+                            'Accept-Encoding': 'gzip, deflate'
+                          },
+                          body: JSON.stringify({
+                            token: token,
+                            email: email
+                          })
+                        })
+}
